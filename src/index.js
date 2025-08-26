ae708c70c8b845f033ec46d9f847fc4dc74f3850
Alberto <lmed@lualmeav.com>
2025-08-25 19:34:10 -0600

    Backend and React router runs on the same server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { PORT } = process.env;
 
 const app = express();
+const buildPath = path.join(__dirname, "build");
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +19,16 @@ app.use("/api", getRoutes);
 app.use("/api", deleteRoutes);
 app.use("/api", postRoutes);
 app.use("/api", putRoutes);
+
+app.use(express.static(buildPath));
+
+app.get('/*any', (req, res) => {
+    if (!req.path.startsWith('/api')) {
+        res.sendFile(buildPath + "/index.html");
+    } else {
+        res.status(404).sendFile("/src/pages/notFound.html");
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on http://localhost:${PORT}`)
